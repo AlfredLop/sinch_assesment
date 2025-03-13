751aa0ee3d921bf8f9b47ab78b29e6f3525c9e02
AlfredLop <alfredo.lopezmeza@gmail.com>
2025-03-13 17:13:52 -0400

    feat: yml based scheme creation working

---
+++ ingestion/schema/load_schema.py
@@ -0,0 +1,35 @@
+import yaml
+import os
+import logging
+from typing import Dict, Any, List
+
+class SchemaLoader:
+    def __init__(self, schema_dir: str = "ingestion/schemas/sinch_db") -> None:
+        self.schema_dir = schema_dir
+
+    def load_tables(self) -> List[Dict[str, Any]]:
+        """Loads all table YAML files and a list of python dics with each table fields and data types."""
+        tables = []
+        try:
+            for filename in os.listdir(self.schema_dir):
+                if filename.endswith(".yml"):
+                    with open(os.path.join(self.schema_dir, filename), "r") as file:
+                        table_data = yaml.safe_load(file)
+                        if "database" not in table_data or "schema" not in table_data:
+                            logging.warning(f"Skipping {filename}: Missing database/schema metadata.")
+                            continue  # Skip files without metadata
+                        tables.append(table_data)
+            print(tables)
+            logging.info("Successfully loaded table schemas.")
+        except FileNotFoundError as e:
+            logging.error(f"Schema directory not found: {e}")
+            raise
+        except yaml.YAMLError as e:
+            logging.error(f"Error parsing YAML: {e}")
+            raise
+        return tables
+
+#this is for testing this standalone script assuming is run in the ingestion folder
+if __name__ == "__main__":
+    test = SchemaLoader("schemas/definitions/sinch_db/")
+    test.load_tables()
